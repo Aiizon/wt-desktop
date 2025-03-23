000fa858d3c324ecf57cdcab0f0b38171c4519e4
Aiizon <killian.bonneau@iia-formation.fr>
2025-03-23 09:39:34 +0100

    Unit form

--- wt-desktop.app/_Unit/UnitForm.axaml.cs
+++ wt-desktop.app/_Unit/UnitForm.axaml.cs
@@ -1,5 +1,8 @@
+using System.Collections.ObjectModel;
+using System.Linq;
 using Avalonia.Controls;
 using wt_desktop.app.Core;
+using wt_desktop.ef;
 using wt_desktop.ef.Entity;
 
 namespace wt_desktop.app;
@@ -23,6 +26,41 @@ public partial class UnitForm : UserControl
 public class UnitFormManager : FormManager<Unit>
 {
     #region Properties
+    private string? _Name;
+    
+    public string? Name
+    {
+        get => _Name;
+        set
+        {
+            _Name = value;
+            OnPropertyChanged();
+        }
+    }
+    
+    private Bay? _SelectedBay;
+    
+    public Bay? SelectedBay
+    {
+        get => _SelectedBay;
+        set
+        {
+            _SelectedBay = value;
+            OnPropertyChanged();
+        }
+    }
+    
+    private ObservableCollection<Bay> _AvailableBays = new();
+    
+    public ObservableCollection<Bay> AvailableBays
+    {
+        get => _AvailableBays;
+        set
+        {
+            _AvailableBays = value;
+            OnPropertyChanged();
+        }
+    }
     #endregion
     
     public UnitFormManager
@@ -30,18 +68,25 @@ public class UnitFormManager : FormManager<Unit>
         UnitController controller, 
         EFormMode      mode, 
         Unit           entity
-    ): base(controller, mode, entity) {
+    ): base(controller, mode, entity)
+    {
+        AvailableBays = new(WtContext.Instance.Set<Bay>().ToList());
+        
         Reset();
     }
 
     public override bool Save()
     {
-
+        CurrentEntity.Name = Name ?? "";
+        CurrentEntity.Bay  = AvailableBays.FirstOrDefault(b => b.Id == SelectedBay?.Id) ?? new Bay();
+        
         return true;
     }
 
     public sealed override void Reset()
     {
+        Name        = CurrentEntity.Name;
+        SelectedBay = AvailableBays.FirstOrDefault(b => b.Id == CurrentEntity.Bay?.Id);
     }
 
     public override bool Cancel()
--- wt-desktop.ef/Entity/Bay.cs
+++ wt-desktop.ef/Entity/Bay.cs
@@ -27,4 +27,9 @@ public class Bay : WtIdentityEntity
     {
         return;
     }
+
+    public override string ToString()
+    {
+        return Name;
+    }
 }
