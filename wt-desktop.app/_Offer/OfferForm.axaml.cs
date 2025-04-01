f8c55dd5eee2bc8532466359f8a0ab35b81ce406
Aizon <killian.bonneau@iia-formation.fr>
2025-04-01 11:50:44 +0200

    Working offer form

--- wt-desktop.app/_Offer/OfferForm.axaml.cs
+++ wt-desktop.app/_Offer/OfferForm.axaml.cs
@@ -26,6 +26,78 @@ public partial class OfferForm : BaseForm
 public class OfferFormManager : FormManager<Offer>
 {
     #region Properties
+
+    private string _Name;
+    
+    public string Name
+    {
+        get => _Name;
+        set
+        {
+            _Name = value;
+            OnPropertyChanged();
+        }
+    }
+
+    private int _MaxUnits;
+    
+    public int MaxUnits
+    {
+        get => _MaxUnits;
+        set
+        {
+            _MaxUnits = value;
+            OnPropertyChanged();
+        }
+    }
+    
+    private string _Availability;
+    
+    public string Availability
+    {
+        get => _Availability;
+        set
+        {
+            _Availability = value;
+            OnPropertyChanged();
+        }
+    }
+    
+    private double _MonthlyRentPrice;
+    
+    public double MonthlyRentPrice
+    {
+        get => _MonthlyRentPrice;
+        set
+        {
+            _MonthlyRentPrice = value;
+            OnPropertyChanged();
+        }
+    }
+    
+    private string _Bandwidth;
+    
+    public string Bandwidth
+    {
+        get => _Bandwidth;
+        set
+        {
+            _Bandwidth = value;
+            OnPropertyChanged();
+        }
+    }
+    
+    private bool _IsActive;
+    
+    public bool IsActive
+    {
+        get => _IsActive;
+        set
+        {
+            _IsActive = value;
+            OnPropertyChanged();
+        }
+    }
     #endregion
     
     public OfferFormManager
@@ -44,14 +116,24 @@ public class OfferFormManager : FormManager<Offer>
             return false;
         }
         
-        
+        CurrentEntity.Name             = Name;
+        CurrentEntity.MaxUnits         = MaxUnits;
+        CurrentEntity.Availability     = Availability;
+        CurrentEntity.MonthlyRentPrice = MonthlyRentPrice;
+        CurrentEntity.Bandwidth        = Bandwidth;
+        CurrentEntity.IsActive         = IsActive;
         
         return true;
     }
 
     public sealed override void Reset()
     {
-        
+        Name             = CurrentEntity.Name;
+        MaxUnits         = CurrentEntity.MaxUnits;
+        Availability     = CurrentEntity.Availability;
+        MonthlyRentPrice = CurrentEntity.MonthlyRentPrice;
+        Bandwidth        = CurrentEntity.Bandwidth;
+        IsActive         = CurrentEntity.IsActive;
     }
 
     public override bool Cancel()
@@ -65,12 +147,38 @@ public class OfferFormManager : FormManager<Offer>
 
         switch (propertyName)
         {
-            
+            case nameof(Name):
+                if (string.IsNullOrWhiteSpace(Name))
+                {
+                    SetError(propertyName, "Le nom ne peut pas être vide.");
+                }
+                break;
+            case nameof(MaxUnits):
+                if (MaxUnits <= 0)
+                {
+                    SetError(propertyName, "Le nombre d'unités doit être supérieur à 0.");
+                }
+                break;
+            case nameof(MonthlyRentPrice):
+                if (MonthlyRentPrice <= 0)
+                {
+                    SetError(propertyName, "Le prix doit être supérieur à 0.");
+                }
+                break;
+            case nameof(Availability):
+                if (string.IsNullOrWhiteSpace(Availability))
+                {
+                    SetError(propertyName, "La disponibilité ne peut pas être vide.");
+                }
+                break;
         }
     }
 
     public override void ValidateForm()
     {
-        
+        ValidateProperty(nameof(Name));
+        ValidateProperty(nameof(MaxUnits));
+        ValidateProperty(nameof(MonthlyRentPrice));
+        ValidateProperty(nameof(Availability));
     }
 }
