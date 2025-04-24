80a4b87b7205df1c035388163f44294bdbdfe0f9
Aizon <killian.bonneau@iia-formation.fr>
2025-04-24 11:59:14 +0200

    Hide filter controls only when there are none

--- wt-desktop.app/Controls/_Form/FilterBox.axaml.cs
+++ wt-desktop.app/Controls/_Form/FilterBox.axaml.cs
@@ -10,18 +10,28 @@ namespace wt_desktop.app.Controls;
 public partial class FilterBox : UserControl
 {
     public static readonly StyledProperty<object> FilterContentProperty =
-        AvaloniaProperty.Register<SearchButton, object> (nameof(FilterContent));
+        AvaloniaProperty.Register<FilterBox, object> (nameof(FilterContent));
 
     public static readonly StyledProperty<ICommand> ResetFilterCommandProperty =
-        AvaloniaProperty.Register<SearchButton, ICommand>(nameof(ResetFilterCommand));
+        AvaloniaProperty.Register<FilterBox, ICommand>(nameof(ResetFilterCommand));
 
     public static readonly StyledProperty<ICommand> ApplyFilterCommandProperty =
-        AvaloniaProperty.Register<SearchButton, ICommand>(nameof(ResetFilterCommand));
+        AvaloniaProperty.Register<FilterBox, ICommand>(nameof(ApplyFilterCommand));
+    
+    public static readonly DirectProperty<FilterBox, bool> HasFiltersProperty =
+        AvaloniaProperty.RegisterDirect<FilterBox, bool>(
+            nameof(HasFilters), 
+            o => o.HasFilters, 
+            (o, v) => o.HasFilters = v);
     
     public object FilterContent
     {
         get => GetValue(FilterContentProperty);
-        set => SetValue(FilterContentProperty, value);
+        set
+        {
+            SetValue(FilterContentProperty, value);
+            UpdateHasFilters(value);
+        }
     }
     
     public ICommand ResetFilterCommand
@@ -35,9 +45,21 @@ public partial class FilterBox : UserControl
         get => GetValue(ApplyFilterCommandProperty);
         set => SetValue(ApplyFilterCommandProperty, value);
     }
+    
+    private bool _hasFilters;
+    public bool HasFilters
+    {
+        get => _hasFilters;
+        set => SetAndRaise(HasFiltersProperty, ref _hasFilters, value);
+    }
 
     public FilterBox()
     {
         InitializeComponent();
+    }
+    
+    private void UpdateHasFilters(object? value)
+    {
+        HasFilters = value != null && !string.Equals(value.ToString(), string.Empty, StringComparison.Ordinal);
     }
 }
